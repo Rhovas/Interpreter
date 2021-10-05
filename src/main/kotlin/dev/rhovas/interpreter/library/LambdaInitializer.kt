02daea3a7346a97b8bebc51283c6fff0ca4732a5
WillBAnders <WillBAnders@gmail.com>
2021-10-05 17:56:56 -0400

    Add Lambda#invoke method for defining functions
    Until function definition is implemented, this allows defining and calling
    functions through lambdas instead. Since function lookup does not support
    varargs bindings have been added manually for arities up to 10.
    
    Functions with trailing lambdas should still invoke the lambda themselves rather
    than through this method to handle implicit parameters.

--- src/main/kotlin/dev/rhovas/interpreter/library/LambdaInitializer.kt
+++ src/main/kotlin/dev/rhovas/interpreter/library/LambdaInitializer.kt
@@ -2,13 +2,24 @@ package dev.rhovas.interpreter.library
 
 import dev.rhovas.interpreter.environment.Function
 import dev.rhovas.interpreter.environment.Object
+import dev.rhovas.interpreter.evaluator.EvaluateException
 import dev.rhovas.interpreter.evaluator.Evaluator
-import dev.rhovas.interpreter.parser.rhovas.RhovasAst
-import java.math.BigInteger
 
 object LambdaInitializer : Library.TypeInitializer("Lambda") {
 
     override fun initialize() {
+        type.methods.define(Function("invoke", 0) { arguments ->
+            val lambda = arguments[0].value as Evaluator.Lambda
+            if (lambda.ast.parameters.size != arguments.size - 1) {
+                throw EvaluateException("Lambda requires ${lambda.ast.parameters} parameter(s), received ${arguments.size}.")
+            }
+            (arguments[0].value as Evaluator.Lambda).invoke(
+                lambda.ast.parameters.zip(arguments.subList(1, arguments.size)).associate { it.first to it.second }
+            )
+        })
+        for (arity in 1..10) {
+            type.methods.define(type.methods["invoke", 0]!!.copy(arity = arity))
+        }
         type.methods.define(Function("toString", 1) { arguments ->
             val lambda = (arguments[0].value as Evaluator.Lambda)
             Object(Library.TYPES["String"]!!, "Lambda/${lambda.ast.parameters.size}#${lambda.hashCode()}")
