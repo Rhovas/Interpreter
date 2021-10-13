97e30b040dc65216d62a456a3cbe64dddf894b17
WillBAnders <WillBAnders@gmail.com>
2021-10-13 00:48:27 -0400

    Replace Lambda#invoke varargs with list

--- src/main/kotlin/dev/rhovas/interpreter/library/LambdaInitializer.kt
+++ src/main/kotlin/dev/rhovas/interpreter/library/LambdaInitializer.kt
@@ -1,25 +1,17 @@
 package dev.rhovas.interpreter.library
 
-import dev.rhovas.interpreter.environment.Function
+import dev.rhovas.interpreter.environment.Object
 import dev.rhovas.interpreter.evaluator.EvaluateException
 import dev.rhovas.interpreter.evaluator.Evaluator
 
 object LambdaInitializer : Library.TypeInitializer("Lambda") {
 
-    override fun initialize() {
-        //TODO: Varargs (prefer List?)
-        type.methods.define(Function("invoke", 0) { arguments ->
-            val lambda = arguments[0].value as Evaluator.Lambda
-            if (lambda.ast.parameters.size != arguments.size - 1) {
-                throw EvaluateException("Lambda requires ${lambda.ast.parameters} parameter(s), received ${arguments.size}.")
-            }
-            (arguments[0].value as Evaluator.Lambda).invoke(
-                lambda.ast.parameters.zip(arguments.subList(1, arguments.size)).associate { it.first to it.second }
-            )
-        })
-        for (arity in 1..10) {
-            type.methods.define(type.methods["invoke", 0]!!.copy(arity = arity))
+    @Reflect.Method("invoke", parameters = ["List"], returns = "")
+    fun invoke(instance: Evaluator.Lambda, arguments: List<Object>): Object {
+        if (instance.ast.parameters.size != arguments.size) {
+            throw EvaluateException("Lambda requires ${instance.ast.parameters.size} parameter(s), received ${arguments.size}.")
         }
+        return instance.invoke(instance.ast.parameters.zip(arguments).associate { it.first to it.second })
     }
 
     @Reflect.Method("toString", returns = "String")
